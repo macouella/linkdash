5b9f6cf0e67497faa3305eec19dca3272f635f41
igimanaloto <igikorn@gmail.com>
2020-04-09 19:40:16 +1200

    fix demo config url

--- api/demo-config.js
+++ api/demo-config.js
@@ -1,6 +1,7 @@
 /**
  * Zeit serverless function, available at linkdash.now.sh/api/demo-config
  */
+const path = require("path");
 const demoConfig = require(path.resolve(__dirname, "../demo/demo.config.js"))();
 
 module.exports = (req, res) => {
