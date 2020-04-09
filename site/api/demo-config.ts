45a59cf618ed07471d010c3cd1b5c6724b698030
Ma Couella <ma@couella.com>
2020-04-10 03:10:10 +1200

    try subdir build

---
+++ site/api/demo-config.ts
@@ -0,0 +1,15 @@
+/**
+ * Zeit serverless function, available at linkdash.now.sh/api/demo-config
+ */
+import { NowRequest, NowResponse } from "@now/node";
+import path from "path";
+const demoConfig = require(path.resolve(__dirname, "../demo/demo.config.js"))();
+
+export default (req: NowRequest, res: NowResponse) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  const { htmlHead, ...config } = demoConfig;
+
+  res.status(200).send(config);
+};
